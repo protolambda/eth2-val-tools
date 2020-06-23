cbcec4d8a85d4fec7c62a5fbb98adc2b99725e0d
protolambda <proto@protolambda.com>
2020-06-23 21:03:27 +0200

    example config

---
+++ example_config.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Warning: bad open passwords, use something else
+# Do not have weak plain passwords like this open for non-test environments.
+# *change these*
+export VALIDATORS_WALLET_PASSWORD="foo"
+export WITHRAWAL_WALLET_PASSWORD="bar"
+export WITHRAWAL_ACC_PASSWORD="quix"
+
+# TODO: insert fork version here, e.g. '0x12abcdef`
+export FORK_VERSION="TODO"
+export WALLET_DIR="./wallets/altona"
+
+# This is where the passwords go for each of the accounts that will be generated
+export ACCOUNT_PASSWORDS_LOC="./wallets/altona/val_passwords.csv"
+
+# Account wallet index start/end, both inclusive.
+export ACC_START_INDEX=1
+export ACC_END_INDEX=15
+
+# TODO: Insert deposit contract address here
+export DEPOSIT_CONTRACT_ADDRESS="TODO"
+export DEPOSIT_DATAS_FILE="./wallets/altona/deposit_datas.txt"
+
+
+# DO NOT DO THIS IN MAINNET
+# With testnets you can be lazy and directly use a Goerli Eth1 keypair
+# *change these*
+export ETH1_FROM_ADDR="0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+export ETH1_FROM_PRIV="0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
